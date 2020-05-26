5119b27a9e2b6416c6baa78ede4427f2fb691d36
CindoddCindy <cindodcindy@gmail.com>
2020-05-26 20:50:05 +0700

    donee

--- online_store/app/src/main/java/cindodcindy/sirihpinang/online_store/MainActivity.java
+++ online_store/app/src/main/java/cindodcindy/sirihpinang/online_store/MainActivity.java
@@ -13,7 +13,6 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import cindodcindy.sirihpinang.online_store.view.ActivityFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView imageView_splash_screen;
      private  int waktu_loading= 4000;
 
     @Override
@@ -21,11 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView_splash_screen=findViewById(R.id.img_awal);
-        Glide.with(MainActivity.this)
-                // LOAD URL DARI LOKAL DRAWABLE
-                .load(R.drawable.tampil_awal)
-                .into(imageView_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
